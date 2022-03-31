25dae603b1be1269767525b45083f7dbb3aa5076
Edgar Ortiz <edortizal@gmail.com>
2022-03-31 05:16:14 +0200

    Modificacion elif de la cuenta de dias

--- practicaHolaMundo/p03.py
+++ practicaHolaMundo/p03.py
@@ -2,7 +2,7 @@ nombre = input("Como te llamas ")
 strEdad = input("Que edad tienes? ")
 strDia = input("Que dia es? ")
 strMes = input("Que mes es? ")
-strAnno = input("Que anno es? ") 
+strAnno = input("Que anno es? ")
 # # strCumplidos = input("Cumpliste ya? ")
 edad = int(strEdad)
 dia = int(strDia)
@@ -12,51 +12,51 @@ prob = int
 trans = int
 if mes == 1:
     prob = (dia / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 2:
     trans = 31 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 3:
     trans = 31 + 28 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 4:
     trans = 31 + 28 + 31 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 5:
     trans = 31 + 28 + 31 + 30 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 6:
     trans = 31 + 28 + 31 + 30 + 31 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 7:
     trans = 31 + 28 + 31 + 30 + 31 + 30 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 8:
     trans = 31 + 28 + 31 + 30 + 31 + 30 + 31 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 9:
     trans = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 10:
     trans = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 11:
-    trans = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + dia 
+    trans = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 elif mes == 12:
-    trans = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + dia 
+    trans = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + dia
     prob = (trans / 365) * 100
-    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ",anno, "es de: ", prob)    
+    print("Hola", nombre, "la probabilidad de que hayas cumplido annos en ", anno, "es de: ", prob)
 else:
     print("Hola", nombre, "has introducido un dato incorrecto")
 
