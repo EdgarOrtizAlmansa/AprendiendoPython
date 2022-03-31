5b732b1bfcf2d01df2d98ec639854cb25bf1c679
Edgar Ortiz <edortizal@gmail.com>
2022-03-31 07:38:38 +0200

    Se corrige error en la entrada de numeros negativos, ahora pueden ser evaluados como numeros validos.

--- practicaHolaMundo/p04.py
+++ practicaHolaMundo/p04.py
@@ -4,7 +4,13 @@ while entero1 == False or entero2 == False:
     str_numero_1 = input('Introduce un numero: ')
     str_numero_2 = input('Introduce un numero: ')
     entero1 = str_numero_1.isdigit()
+    if entero1 == False and str_numero_1[0] == '-':
+        numero_1 = int(str_numero_1)
+        entero1 = True
     entero2 = str_numero_2.isdigit()
+    if entero2 == False and str_numero_2[0] == '-':
+        numero_2 = int(str_numero_1)
+        entero2 = True
 
 numero_1 = int(str_numero_1)
 numero_2 = int(str_numero_2)
