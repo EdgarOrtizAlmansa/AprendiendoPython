6e5c7f593fcafe75166bed2a95e6e160cc9bc86e
Edgar Ortiz <edortizal@gmail.com>
2022-03-31 06:19:03 +0200

    Se crea la p04.py, ejercicio practico 1, se crea el bucle para la peticion de dos numeros enteros y se hace la comprobacion

--- practicaHolaMundo/p04.py
+++ practicaHolaMundo/p04.py
@@ -1,2 +1,10 @@
-str_numero_1 = input('Introduce un numero: )
-str_numero_2 = input('Introduce un numero: )
+comp = True
+comp2 = False
+
+while comp != comp2:
+    str_numero_1 = input('Introduce un numero: ' )
+    str_numero_2 = input('Introduce un numero: ' )
+    comp = str_numero_1.isdigit()
+    comp2 = str_numero_2.isdigit()
+
+
