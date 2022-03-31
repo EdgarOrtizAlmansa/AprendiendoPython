afd5a6059ccd7c1dd62dd4ab75f4bf6ca5fcb4c0
Edgar Ortiz <edortizal@gmail.com>
2022-03-31 06:58:47 +0200

    Correccion de fallos en el operador logico (!=), Resolucion del ejercicio, comprobando el correcto funcionamiento de la suma, resta, mult, div y dando formato a la salida decimal

--- practicaHolaMundo/p04.py
+++ practicaHolaMundo/p04.py
@@ -1,10 +1,18 @@
-comp = True
-comp2 = False
+entero1 = False
+entero2 = False
+while entero1 == False or entero2 == False:
+    str_numero_1 = input('Introduce un numero: ')
+    str_numero_2 = input('Introduce un numero: ')
+    entero1 = str_numero_1.isdigit()
+    entero2 = str_numero_2.isdigit()
 
-while comp != comp2:
-    str_numero_1 = input('Introduce un numero: ' )
-    str_numero_2 = input('Introduce un numero: ' )
-    comp = str_numero_1.isdigit()
-    comp2 = str_numero_2.isdigit()
+numero_1 = int(str_numero_1)
+numero_2 = int(str_numero_2)
 
+numero_1 = numero_1 / 10
+numero_2 = numero_2 / 10
 
+print('La suma de los numeros es: ', round(numero_1 + numero_2, 2))
+print('La resta de los numeros es: ', round(numero_1 - numero_2, 2))
+print('La multiplicacion de los numeros es: ', round(numero_1 * numero_2, 2))
+print('La division de los numeros es: ', round(numero_1 / numero_2, 2))
