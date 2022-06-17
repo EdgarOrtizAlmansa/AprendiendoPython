752c17a5a78b793a4f76935a22ccc7ac70acc72a
Edgar Ortiz <edortizal@gmail.com>
2022-06-17 06:56:55 +0200

    cambios realizados en todas las practicas

--- practicaHolaMundo/p04.py
+++ practicaHolaMundo/p04.py
@@ -4,14 +4,14 @@ while entero1 == False or entero2 == False:
     str_numero_1 = input('Introduce un numero: ')
     str_numero_2 = input('Introduce un numero: ')
     entero1 = str_numero_1.isdigit()
-    if entero1 == False and str_numero_1[0] == '-':
+    if entero1 == False and str_numero_1[0] == '-' and str_numero_1[1:].isdigit():
         numero_1 = int(str_numero_1)
         entero1 = True
     entero2 = str_numero_2.isdigit()
-    if entero2 == False and str_numero_2[0] == '-':
-        numero_2 = int(str_numero_1)
+    if entero2 == False and str_numero_2[0] == '-' and str_numero_2[1:].isdigit():
+        numero_2 = int(str_numero_2)
         entero2 = True
-
+    print('Uno de los valores introducidos no es correcto.')
 numero_1 = int(str_numero_1)
 numero_2 = int(str_numero_2)
 
